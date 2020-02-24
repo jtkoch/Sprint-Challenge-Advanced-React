0438ccd228c499646152e835ae92b8ea3a0cd7f1
Jensen Koch <jtkoch12@gmail.com>
2020-02-23 20:21:37 -0600

    Finished MVP and Stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import Players from './components/Players';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sprint-Challenge-Advanced-React</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      array: [{
+        id: "",
+        name: "test",
+        country: "",
+        searches: ""
+      }]
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        console.log(res.data)
+        this.setState({array: res.data})
+      })
+    }
+
+  render() {
+    console.log(this.state.array)
+    return(
+      <div className="App">
+        <Navbar />
+        <div className="player-card">
+          {this.state.array.map((info) => {
+            return(
+              <Players id={info.id} key={info.id} name={info.name} country={info.country} searches={info.searches} />
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
